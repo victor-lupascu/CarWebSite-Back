cf5b7b97f1b1cad9ff3598a78a9506c6a83746e1
Dragos <bivoldragos6@gmail.com>
2026-03-31 01:31:39 +0300

    feat: add SQL Server connection, appsettings, Program.cs, EF migrations

--- CarWebSite.Api/Program.cs
+++ CarWebSite.Api/Program.cs
@@ -1,6 +1,22 @@
+using CarWebSite.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
