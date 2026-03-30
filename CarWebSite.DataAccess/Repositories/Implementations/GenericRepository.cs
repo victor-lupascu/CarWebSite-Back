482f7c325c0814ce82d02590c1d6d433baf84d0f
Dragos <bivoldragos6@gmail.com>
2026-03-31 01:13:19 +0300

    feat: add DataAccess layer - AppDbContext, GenericRepository

---
+++ CarWebSite.DataAccess/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using CarWebSite.DataAccess.Context;
+using CarWebSite.DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWebSite.DataAccess.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() =>
+            await _dbSet.ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id) =>
+            await _dbSet.FindAsync(id);
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
