22f2061c3f876dae67790e878e5279e820428313
victor lupascu <victor.lupascu26@gmail.com>
2026-04-02 00:51:14 +0300

    Add ContactMessageData, FavoriteData entities and ContactSubject enum

---
+++ CarWebSite.Domain/Entities/FavoriteData.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarWebSite.Domain.Entities
+{
+    public class FavoriteData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserDataId { get; set; }
+        public UserData UserData { get; set; } = null!;
+
+        [Required]
+        public int CarId { get; set; }
+        public Car Car { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
