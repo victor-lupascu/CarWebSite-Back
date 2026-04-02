a8c97b4519620dadf6d30ca33c63c63a48b4bda4
Dragos <bivoldragos6@gmail.com>
2026-04-02 21:23:38 +0300

    feat: add CarImage and ContactMessage repositories

---
+++ CarWebSite.DataAccess/Repositories/Implementations/CarImageRepository.cs
@@ -0,0 +1,19 @@
+﻿using CarWebSite.DataAccess.Context;
+using CarWebSite.DataAccess.Repositories.Interfaces;
+using CarWebSite.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWebSite.DataAccess.Repositories.Implementations
+{
+    public class CarImageRepository : GenericRepository<CarImage>, ICarImageRepository
+    {
+        public CarImageRepository(AppDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<CarImage>> GetByCarIdAsync(int carId)
+        {
+            return await _context.CarImages
+                .Where(ci => ci.CarId == carId)
+                .ToListAsync();
+        }
+    }
+}
---
+++ CarWebSite.DataAccess/Repositories/Interfaces/IContactMessageRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarWebSite.Domain.Entities;
+
+namespace CarWebSite.DataAccess.Repositories.Interfaces
+{
+    public interface IContactMessageRepository : IGenericRepository<ContactMessageData>
+    {
+        Task<IEnumerable<ContactMessageData>> GetByEmailAsync(string email);
+    }
+}
