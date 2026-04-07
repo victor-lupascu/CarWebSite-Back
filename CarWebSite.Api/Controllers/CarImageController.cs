c7669c000ff6c52cf698a2bab031f271ed9d82ed
AndrreyG <andreigandrabur6@gmail.com>
2026-04-07 20:33:07 +0300

    created CarImageController

---
+++ CarWebSite.Api/Controllers/CarImageController.cs
@@ -0,0 +1,39 @@
+using CarWebSite.BusinessLayer.Interfaces;
+using CarWebSite.Domain.Models.CarImage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWebSite.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarImageController : ControllerBase
+    {
+        private readonly ICarImageAction _carImageAction;
+
+        public CarImageController(ICarImageAction carImageAction)
+        {
+            _carImageAction = carImageAction;
+        }
+
+        [HttpGet("{carId}")]
+        public async Task<IActionResult> GetByCarId(int carId)
+        {
+            var images = await _carImageAction.GetImagesByCarAction(carId);
+            return Ok(images);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CarImageCreateDto data)
+        {
+            var response = await _carImageAction.AddImageAction(data);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _carImageAction.DeleteImageAction(id);
+            return Ok(response);
+        }
+    }
+}
