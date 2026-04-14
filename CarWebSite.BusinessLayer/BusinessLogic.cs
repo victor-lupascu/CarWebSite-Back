a0469de292629837108ce27900929de6048baebd
victor lupascu <victor.lupascu26@gmail.com>
2026-04-14 21:51:09 +0300

    Add BrandExecution with inheritance and BusinessLogic entry point

---
+++ CarWebSite.BusinessLayer/BusinessLogic.cs
@@ -0,0 +1,15 @@
+﻿using CarWebSite.BusinessLayer.Interfaces;
+using CarWebSite.BusinessLayer.Structure;
+
+namespace CarWebSite.BusinessLayer
+{
+    public class BusinessLogic
+    {
+        public BusinessLogic() { }
+
+        public IBrandAction BrandAction()
+        {
+            return new BrandExecution();
+        }
+    }
+}
---
+++ CarWebSite.BusinessLayer/Structure/BrandExecution.cs
@@ -0,0 +1,30 @@
+﻿using CarWebSite.BusinessLayer.Core;
+using CarWebSite.BusinessLayer.Interfaces;
+using CarWebSite.Domain.Models.Brand;
+using CarWebSite.Domain.Models.Responses;
+
+namespace CarWebSite.BusinessLayer.Structure
+{
+    public class BrandExecution : BrandActions, IBrandAction
+    {
+        public List<BrandResponseDto> GetAllBrandsAction()
+        {
+            return GetAllBrandsActionExecution();
+        }
+
+        public BrandResponseDto? GetBrandByIdAction(int id)
+        {
+            return GetBrandByIdActionExecution(id);
+        }
+
+        public ActionResponse CreateBrandAction(BrandCreateDto data)
+        {
+            return CreateBrandActionExecution(data);
+        }
+
+        public ActionResponse DeleteBrandAction(int id)
+        {
+            return DeleteBrandActionExecution(id);
+        }
+    }
+}
