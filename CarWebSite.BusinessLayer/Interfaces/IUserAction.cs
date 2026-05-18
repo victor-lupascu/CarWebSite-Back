146f76845b052cf4da254265a2b8d178b804b790
victor lupascu <victor.lupascu26@gmail.com>
2026-05-18 13:31:33 +0300

    Add IUserAction and UserExecution for user auth actions

---
+++ CarWebSite.BusinessLayer/Interfaces/IUserAction.cs
@@ -0,0 +1,13 @@
+﻿using CarWebSite.Domain.Models.Responses;
+using CarWebSite.Domain.Models.User;
+
+namespace CarWebSite.BusinessLayer.Interfaces
+{
+    public interface IUserAction
+    {
+        ActionResponse Register(UserRegisterDto dto);
+        AuthResponse Login(UserLoginDto dto);
+        AuthResponse Refresh(string refreshToken);
+        ActionResponse Logout(string refreshToken);
+    }
+}
---
+++ CarWebSite.BusinessLayer/Structure/UserExecution.cs
@@ -0,0 +1,30 @@
+﻿using CarWebSite.BusinessLayer.Core;
+using CarWebSite.BusinessLayer.Interfaces;
+using CarWebSite.Domain.Models.Responses;
+using CarWebSite.Domain.Models.User;
+
+namespace CarWebSite.BusinessLayer.Structure
+{
+    public class UserExecution : UserActions, IUserAction
+    {
+        public ActionResponse Register(UserRegisterDto dto)
+        {
+            return RegisterUserActionExecution(dto);
+        }
+
+        public AuthResponse Login(UserLoginDto dto)
+        {
+            return LoginUserActionExecution(dto);
+        }
+
+        public AuthResponse Refresh(string refreshToken)
+        {
+            return RefreshTokenActionExecution(refreshToken);
+        }
+
+        public ActionResponse Logout(string refreshToken)
+        {
+            return LogoutActionExecution(refreshToken);
+        }
+    }
+}
