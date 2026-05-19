384560638568756eb242fc80ea29dafdde8012b5
victor lupascu <victor.lupascu26@gmail.com>
2026-05-19 20:42:23 +0300

    Add RefreshTokenRequestDto for refresh and logout endpoints

---
+++ CarWebSite.Domain/Models/User/RefreshTokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWebSite.Domain.Models.User
+{
+    public class RefreshTokenRequestDto
+    {
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
