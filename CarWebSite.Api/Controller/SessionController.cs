2eae36085eb14972c3c4e9e34f6085695f5808c1
victor lupascu <victor.lupascu26@gmail.com>
2026-05-19 20:44:30 +0300

    Add SessionController with register, login, refresh and logout endpoints

---
+++ CarWebSite.Api/Controller/SessionController.cs
@@ -0,0 +1,51 @@
+﻿using CarWebSite.BusinessLayer;
+using CarWebSite.BusinessLayer.Interfaces;
+using CarWebSite.Domain.Models.User;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarWebSite.Api.Controller
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionController : ControllerBase
+    {
+        private readonly IUserAction _userAction;
+
+        public SessionController()
+        {
+            var bl = new BusinessLogic();
+            _userAction = bl.UserAction();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserRegisterDto dto)
+        {
+            var response = _userAction.Register(dto);
+            return Ok(response);
+        }
+
+        [HttpPost("auth")]
+        public IActionResult Login([FromBody] UserLoginDto dto)
+        {
+            var response = _userAction.Login(dto);
+            return Ok(response);
+        }
+
+        [HttpPost("refresh")]
+        public IActionResult Refresh([FromBody] RefreshTokenRequestDto dto)
+        {
+            var response = _userAction.Refresh(dto.RefreshToken);
+            return Ok(response);
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout([FromBody] RefreshTokenRequestDto dto)
+        {
+            var response = _userAction.Logout(dto.RefreshToken);
+            return Ok(response);
+        }
+
+    }
+}
